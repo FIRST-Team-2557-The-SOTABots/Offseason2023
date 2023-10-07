acc5390de906d512600703a8e271a4c096f98323
HoJo42 <howardwalz@gmail.com>
2023-10-07 11:45:50 -0700

    Rotation Works, no commands

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -22,6 +22,8 @@ import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import frc.robot.Constants.OIConstants;
+import frc.robot.commands.RotationPID;
+import frc.robot.commands.RotationPID.RotationSetpoint;
 import frc.robot.configs.SuperStructureConfig;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.Extension;
@@ -33,13 +35,14 @@ public class RobotContainer {
 
   private ConfigUtils configUtils;
   private SOTA_Xboxcontroller dController = new SOTA_Xboxcontroller(0);
+  private SOTA_Xboxcontroller mController = new SOTA_Xboxcontroller(1);
   private final DriveSubsystem mDriveTrain;
   private boolean driveFieldCentric = true; // REV code is trash and we have to manage this out here
   private Extension mExtension;
   private Rotation mRotation;
   private Intake mIntake;
   private SuperStructure superStructure;
-  // private RotationPID rotationPID;
+  private RotationPID rotationPID;
   // private ExtensionPID extensionPID;
 
   public RobotContainer() {
@@ -49,39 +52,45 @@ public class RobotContainer {
 
     try {
       SOTA_MotorController rotationMotor = MotorControllerFactory.generateMotorController(
-        (configUtils.readFromClassPath(MotorControllerConfig.class, "SuperStructure/RotatorMotor")));
+          (configUtils.readFromClassPath(MotorControllerConfig.class, "SuperStructure/RotatorMotor")));
 
-        SOTA_AbsoulteEncoder rotatiEncoder = EncoderFactory.generateAbsoluteEncoder(configUtils.readFromClassPath(EncoderConfig.class, "SuperStructure/RotationEncoder"));
-  
-        SOTA_MotorController winchMotor = MotorControllerFactory.generateMotorController
-        (configUtils.readFromClassPath(MotorControllerConfig.class, "SuperStructure/WinchMotor"));
-  
-        // SOTA_MotorController intakeMotorTop = MotorControllerFactory.generateMotorController
-        // (configUtils.readFromClassPath(MotorControllerConfig.class, "SuperStructure/IntakeMotorTop"));
-        // SOTA_MotorController intakeMotorBottom = MotorControllerFactory.generateMotorController
-        // (configUtils.readFromClassPath(MotorControllerConfig.class, "SuperStructure/IntakeMotorBottom"));
-  
-  
-        DigitalInput limitSwitch = new DigitalInput(0);
-  
-        SuperStructureConfig superStructureConfig = configUtils.readFromClassPath(SuperStructureConfig.class,
-         "SuperStructure/SuperStructure");
-  
-        this.mExtension = new Extension(winchMotor, limitSwitch, superStructureConfig);
-        this.mRotation = new Rotation(rotationMotor, rotatiEncoder,superStructureConfig);
-        // this.mIntake = new Intake(intakeMotorTop, intakeMotorBottom);
-  
-        this.superStructure = new SuperStructure(mExtension::getLength, mRotation::getRotationDegrees, superStructureConfig);
-  
-  
-        ProfiledPIDController extensController = new ProfiledPIDController(5, 0, 0,
-         new TrapezoidProfile.Constraints(60.0,100.0));
-  
-        // this.rotationPID = new RotationPID(mRotation, mExtension::getLengthFromStart, superStructure::minRotation, superStructure::maxRotation, superStructureConfig);
-        // this.extensionPID = new ExtensionPID(extensController, mExtension, superStructure::maxExtension);
-        // this.mResetExtension = new ResetExtension(mExtension);
-        // this.intakeCommand = new BasicIntakeCommand(mIntake, mController::getLeftY);
-      
+      SOTA_AbsoulteEncoder rotatiEncoder = EncoderFactory.generateAbsoluteEncoder(
+          configUtils.readFromClassPath(EncoderConfig.class, "SuperStructure/RotationEncoder"));
+
+      SOTA_MotorController winchMotor = MotorControllerFactory.generateMotorController(
+          configUtils.readFromClassPath(MotorControllerConfig.class, "SuperStructure/WinchMotor"));
+
+      // SOTA_MotorController intakeMotorTop =
+      // MotorControllerFactory.generateMotorController
+      // (configUtils.readFromClassPath(MotorControllerConfig.class,
+      // "SuperStructure/IntakeMotorTop"));
+      // SOTA_MotorController intakeMotorBottom =
+      // MotorControllerFactory.generateMotorController
+      // (configUtils.readFromClassPath(MotorControllerConfig.class,
+      // "SuperStructure/IntakeMotorBottom"));
+
+      DigitalInput limitSwitch = new DigitalInput(0);
+
+      SuperStructureConfig superStructureConfig = configUtils.readFromClassPath(SuperStructureConfig.class,
+          "SuperStructure/SuperStructure");
+
+      this.mExtension = new Extension(winchMotor, limitSwitch, superStructureConfig);
+      this.mRotation = new Rotation(rotationMotor, rotatiEncoder, superStructureConfig);
+      // this.mIntake = new Intake(intakeMotorTop, intakeMotorBottom);
+
+      this.superStructure = new SuperStructure(mExtension::getLength, mRotation::getRotationDegrees,
+          superStructureConfig);
+
+      ProfiledPIDController extensController = new ProfiledPIDController(5, 0, 0,
+          new TrapezoidProfile.Constraints(60.0, 100.0));
+
+      this.rotationPID = new RotationPID(mRotation, mExtension::getLengthFromStart, superStructure::minRotation,
+          superStructure::maxRotation, superStructureConfig);
+      // this.extensionPID = new ExtensionPID(extensController, mExtension,
+      // superStructure::maxExtension);
+      // this.mResetExtension = new ResetExtension(mExtension);
+      // this.intakeCommand = new BasicIntakeCommand(mIntake, mController::getLeftY);
+
     } catch (Exception e) {
       e.printStackTrace();
     }
@@ -97,6 +106,8 @@ public class RobotContainer {
             -MathUtil.applyDeadband(dController.getRightX(), OIConstants.kDriveDeadband),
             driveFieldCentric, false),
         mDriveTrain));
+
+    mRotation.setDefaultCommand(rotationPID);
   }
 
   private void configureBindings() {
@@ -112,8 +123,9 @@ public class RobotContainer {
           }
           SmartDashboard.putBoolean("FieldCentric", driveFieldCentric);
         }, mDriveTrain));
-  }
 
+    mController.a().onTrue(new InstantCommand(() -> rotationPID.setSetpoint(RotationSetpoint.SUBSTATION), mRotation));
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
---
+++ src/main/java/frc/robot/commands/RotationPID.java
@@ -0,0 +1,87 @@
+package frc.robot.commands;
+
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.configs.SuperStructureConfig;
+import frc.robot.subsystems.Rotation;
+
+public class RotationPID extends CommandBase{
+
+    public enum RotationSetpoint {
+        RESET(150),
+        REST(175),
+        FLOOR(49),
+        FLOORCONE(55),
+        FLOORCONEKNOCK(69),
+        HIGH(118),
+        MID(110),
+        SUBSTATION(135),
+        SINGLE(98);
+    
+        public double degrees;
+    
+        private RotationSetpoint(double degrees) {
+            this.degrees = degrees;
+        }
+    
+    }
+
+    private Rotation mRotation;
+    private double setpoint;
+    private PIDController pidController;
+    private double kP;
+    private double kPG; // proportional gain against gravity
+    private DoubleSupplier kMinAngle;
+    private DoubleSupplier kMaxAngle;
+    private DoubleSupplier kExtensionlength;
+    private double kRotationDelta;
+    private double kRotationDeltaProportional;
+    private double kMaxExtension;
+
+    public RotationPID(Rotation rotation,
+            DoubleSupplier extensionLength,
+            DoubleSupplier minAngle, 
+            DoubleSupplier maxAngle,
+            SuperStructureConfig config){
+        this.mRotation = rotation; 
+        this.setpoint = config.getRotationInitSetpoint();
+        this.pidController = config.getRotationPIDController();
+        this.kP = config.getRotationKP();
+        this.kPG = config.getRotationKPG();
+        this.kExtensionlength = extensionLength; 
+        this.kMinAngle = minAngle; 
+        this.kMaxAngle = maxAngle; 
+        this.kRotationDelta = config.getRotationDelta();
+        this.kRotationDeltaProportional = config.getRotationDeltaProportional();
+        this.kMaxExtension = config.getMaxExtension();
+        addRequirements(rotation);
+    }
+
+    public void setSetpoint(RotationSetpoint newSetpoint) {
+        setpoint = newSetpoint.degrees;
+    }
+
+    public boolean atSetpoint(){
+        return Math.abs(setpoint - mRotation.getRotationDegrees()) < 1; 
+    }
+    
+
+    @Override
+    public void execute() {
+        double adjustedSetpoint = MathUtil.clamp(setpoint, kMinAngle.getAsDouble(), kMaxAngle.getAsDouble());
+
+        pidController.setP(kPG - ((kP * kExtensionlength.getAsDouble()) / kMaxExtension));
+
+        double output = Math.sin(mRotation.getRotationRadians()) * (kRotationDelta + (kRotationDeltaProportional * kExtensionlength.getAsDouble() / kMaxExtension)) 
+        + pidController.calculate(mRotation.getRotationDegrees(), adjustedSetpoint);
+
+        mRotation.set(output);
+
+
+    }
+    
+}
